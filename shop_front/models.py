b0d03539b713f9c177b1cb58d46a6e6b554e061e
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-09-23 02:02:58 +0100

    fix: database

--- shop_front/models.py
+++ shop_front/models.py
@@ -14,6 +14,8 @@ CATEGORY = (
 
 class Item(models.Model):
     item_name = models.CharField(max_length=100)
+    item_sub_name = models.CharField(max_length=100)
+
     category = models.CharField(choices=CATEGORY, max_length=2)
 
     def __str__(self):
