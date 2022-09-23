2b73171fc47795d8ccd028909cadbcacccb223d3
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-09-23 01:22:52 +0100

    fix: admin area

--- users_app/models.py
+++ users_app/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
