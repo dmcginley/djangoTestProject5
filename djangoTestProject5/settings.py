03856373aa332ef6275fb56014ee7b7bdeefb654
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-09-23 01:34:51 +0100

    fix: middleware app name

--- djangoTestProject5/settings.py
+++ djangoTestProject5/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'techblog_app.middleware.TimezoneMiddleware',
+    'shop_front.middleware.TimezoneMiddleware',
 
 ]
 
